b782eee94254cd19c5da36fbdb938e7c0088a5c8
Andrew Matheus <andrewmatheus@gmail.com>
2020-01-21 14:54:07 -0300

    configuracoes atuais

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://10.0.2.2:3333',
+  baseURL: 'http://172.28.10.209:3333', // usando emulador 10.0.2.2:3333
 });
 
 export default api;
