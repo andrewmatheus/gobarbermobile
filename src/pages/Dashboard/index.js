eb548d3b43dd625ebc3dbd281e796038efa5e835
Andrew Matheus <andrewmatheus@gmail.com>
2020-01-26 17:57:13 -0300

    GoBarberApp, finalizado!

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,28 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-
+import { withNavigationFocus } from 'react-navigation';
 import PropTypes from 'prop-types';
+
+import api from '~/services/api';
+
 import Background from '~/components/Background';
 import Appointment from '~/components/Appointment';
 
 import { Container, Title, List } from './styles';
 
-const data = [1, 2, 3, 4, 5];
-
 const barIcon = ({ tintColor }) => (
   <Icon name="event" size={20} color={tintColor} />
 );
+function Dashboard({ isFocused }) {
+  const [appointments, setAppointments] = useState([]);
 
-export default function Dashboard() {
+  async function loadAppointments() {
+    const response = await api.get('appointments');
+
+    setAppointments(response.data);
+  }
+
+  useEffect(() => {
+    if (isFocused) {
+      loadAppointments();
+    }
+  }, [isFocused]);
+
+  async function handleCancel(id) {
+    const response = await api.delete(`appointments/${id}`);
+
+    setAppointments(
+      appointments.map(appointment =>
+        appointment.id === id
+          ? {
+              ...appointment,
+              canceled_at: response.data.canceled_at,
+            }
+          : appointment
+      )
+    );
+  }
+
   return (
     <Background>
       <Container>
         <Title>Agendamentos</Title>
 
         <List
-          data={data}
-          keyExtractor={item => String(item)}
-          renderItem={({ item }) => <Appointment data={item} />}
+          data={appointments}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => (
+            <Appointment onCancel={() => handleCancel(item.id)} data={item} />
+          )}
         />
       </Container>
     </Background>
@@ -37,3 +68,5 @@ Dashboard.navigationOptions = {
 barIcon.propTypes = {
   tintColor: PropTypes.string.isRequired,
 };
+
+export default withNavigationFocus(Dashboard);
